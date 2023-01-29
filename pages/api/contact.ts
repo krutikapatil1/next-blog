885ddcec02353a242498b6652b480de398e01fc5
krutikapatil1 <krutiks89@gmail.com>
2023-01-29 12:13:19 +0530

    Fixes #9 Added logic to save a new contact to mongodb

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -1,4 +1,13 @@
-const handler = (req: any, res: any) => {
+import { MongoClient } from "mongodb";
+
+interface newMessageProps {
+  id?: string;
+  email: string;
+  name: string;
+  message: string;
+}
+
+const handler = async (req: any, res: any) => {
   if (req.method === "POST") {
     const { email, name, message } = req.body;
     if (
@@ -13,12 +22,32 @@ const handler = (req: any, res: any) => {
       return;
     }
 
-    const newMessage = {
+    const newMessage: newMessageProps = {
       email,
       name,
       message,
     };
 
+    let client;
+
+    try {
+      client = await MongoClient.connect(
+        "mongodb+srv://nextJsPractice:nextJsPractice34@cluster0.h3k9eua.mongodb.net/my-site?retryWrites=true&w=majority"
+      );
+    } catch (error) {
+      res.status(500).json({ message: "Error conneciton to the db" });
+      return;
+    }
+
+    const db = client.db("my-site");
+    try {
+      const result = await db.collection("contact").insertOne(newMessage);
+      newMessage.id = result.insertedId.toString();
+    } catch (error) {
+      res.status(500).json({ message: "Error adding the data to the db" });
+    }
+
+    client.close();
     console.log(newMessage);
     res.status(201).json({ message: "New message added", payload: newMessage });
   }
